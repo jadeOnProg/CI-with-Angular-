ee55fa2050837605d43702b00e40d2e394aaddec
jadeOnProg <jadeonprog@gmail.com>
2017-05-22 23:22:02 +0800

    clean code
    clean the codes into something :D

--- application/models/Object_model.php
+++ application/models/Object_model.php
@@ -13,17 +13,20 @@ class Object_model extends CI_Model {
 		return $query->result();
 	}
 	
+	public function get_object($id)
 	{
 		$this->db->where('id',$id);
 		$query = $this->db->get('tbl_objects');
 		return $query->result();
 	}
 	
+	public function delete_object($id)
 	{
 		$this->db->where('id',$id);
 		$this->db->delete('tbl_objects');
 	}
 	
+	public function update_ojbect($id, $data)
 	{
 		$this->db->where('id',$id);
 		$this->db->update('tbl_objects',$data);
